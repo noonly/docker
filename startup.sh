3e025316d8ca79d75fb31425a2167a79c04b1d0c
xiaole <x@noonly.com>
2016-07-09 09:01:12 +0800

    settings redis configure file and add startup alert

--- startup.sh
+++ startup.sh
@@ -22,7 +22,7 @@ if [ "_$tmp" == '_y' ]; then
 	do
         	#echo ""
         	#echo "invalid container name! please retry!!!"
-       		read -p "Please enter the address of any node in the cluster: " tmp
+       		read -p "Please enter the IP address of any node in the cluster: " tmp
 		clusterip=$tmp	
 
 	done
@@ -48,8 +48,8 @@ do
 
 
 done
-
-CONTAINERNAME[0]=$tmp
+index=0
+CONTAINERNAME[$index]=$tmp
 echo "Your consul master container $tmp is running"
 #run consul master node
 if [ "_$clusterip" != "_" ]; then
@@ -63,123 +63,152 @@ CONSUL_IP=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $tmp)
 tmp=""
 #read -p "Please setting the redis master node docker container name: " tmp
 
-while [ "_$tmp" == '_' ]
-do
-	read -p "Please settings the redis master container name: " tmp
-	echo ""
-	 for exist in $CONTAINERNAME
-        do
-                if [ "x$tmp" == "x$exist" ]; then
+read -p "Do you want to startup redis master server? [y/N]" y
 
-                        echo "invalid container name! please retry!!!"
-                        tmp=""
-                fi
-        done
+if [ "_$y" == "_y" ]; then
+	y=""
+	while [ "_$tmp" == '_' ]
+	do
+		read -p "Please settings the redis master container name: " tmp
+		for exist in $CONTAINERNAME
+			do
+					if [ "x$tmp" == "x$exist" ]; then
 
-        #echo "invalid container name! please retry!!!\n"
-	#read -p "Please setting the redis master node docker container name: " tmp
-done
+							echo "invalid container name! please retry!!!"
+							tmp=""
+					fi
+			done
 
-CONTAINERNAME[1]=$tmp
-echo "Your redis master container $tmp is running"
+			#echo "invalid container name! please retry!!!\n"
+		#read -p "Please setting the redis master node docker container name: " tmp
+	done
 
-#run redis master server
-docker run -d -v /etc/localtime:/etc/localtime:ro -v $BASEDIR/redis/redis.conf.bak:/usr/local/etc/redis/redis.conf -v $BASEDIR/redis/service.json:/etc/consul.d/service1.json \
---name $tmp --hostname $tmp --env REDISCONF=/usr/local/etc/redis/redis.conf --env CONSUL_HOST=$CONSUL_IP noonly/redis
+	index=$(($index+1))
+	CONTAINERNAME[$index]=$tmp
+	echo "Your redis master container $tmp is running"
+
+	#run redis master server
+	docker run -d -v /etc/localtime:/etc/localtime:ro -v $BASEDIR/redis/redis.conf.bak:/usr/local/etc/redis/redis.conf -v $BASEDIR/redis/service.json:/etc/consul.d/service1.json \
+	--name $tmp --hostname $tmp --env REDISCONF=/usr/local/etc/redis/redis.conf --env CONSUL_HOST=$CONSUL_IP noonly/redis
+	tmp=""
+fi
 
 
-tmp=""
 #read -p "Please setting the redis slave node docker container name: " tmp
+read -p "Do you want to startup redis slave server? [y/N]" y
 
-while [ "_$tmp" == '_' ]
-do
-	#echo "\n"
-        #echo "invalid container name! please retry!!!\n"
-        read -p "Please settings the redis slave container name: " tmp
-	echo ""
-        for exist in $CONTAINERNAME
-        do
-		if [ "x$tmp" == "x$exist" ]; then
+if [ "_$y" == "_y" ]; then
+	y=""
+	while [ "_$tmp" == '_' ]
+	do
+		#echo "\n"
+			#echo "invalid container name! please retry!!!\n"
+			read -p "Please settings the redis slave container name: " tmp
+			for exist in $CONTAINERNAME
+			do
+			if [ "x$tmp" == "x$exist" ]; then
 
-                	echo "invalid container name! please retry!!!"
-                	tmp=""
-		fi
-        done
+						echo "invalid container name! please retry!!!"
+						tmp=""
+			fi
+			done
 
-done
+	done
 
-CONTAINERNAME[2]=$tmp
+	index=$(($index+1))
+	CONTAINERNAME[$index]=$tmp
 
-echo "Your redis slave container $tmp is running"
-#run redis slave server
-docker run -v /etc/localtime:/etc/localtime:ro --link ${CONTAINERNAME[1]}:redis-master --hostname $tmp -v $BASEDIR/redis/redis.conf:/usr/local/etc/redis/redis.conf \
---env REDISCONF=/usr/local/etc/redis/redis.conf --env CONSUL_HOST=$CONSUL_IP --name $tmp -d noonly/redis
+	echo "Your redis slave container $tmp is running"
+	#run redis slave server
+	docker run -v /etc/localtime:/etc/localtime:ro --hostname $tmp -v $BASEDIR/redis/redis.conf:/usr/local/etc/redis/redis.conf \
+	--env REDISCONF=/usr/local/etc/redis/redis.conf --env CONSUL_HOST=$CONSUL_IP --name $tmp -d noonly/redis
 
-tmp=""
-
-while [ "_$tmp" == '_' ]
-do
-        read -p "Please settings the consul slave container name: " tmp
-	 echo ""
-        for exist in $CONTAINERNAME
-        do
-                if [ "x$tmp" == "x$exist" ]; then
-
-                        echo "invalid container name! please retry!!!"
-                        tmp=""
-                fi
-        done
-
-done
-
-CONTAINERNAME[3]=$tmp
-
-echo "Your consul slave container $tmp is running"
-#run consul client for master node
-docker run -d --name $tmp --hostname $tmp -v /etc/localtime:/etc/localtime:ro --env CONSUL_HOST=$CONSUL_IP noonly/consul
-
-tmp=""
-
-while [ "_$tmp" == '_' ]
-do
-        read -p "Please settings the nginx server container name: " tmp
-	 echo ""
-        for exist in $CONTAINERNAME
-        do
-                if [ "x$tmp" == "x$exist" ]; then
-
-                        echo "invalid container name! please retry!!!"
-                        tmp=""
-                fi
-        done
-
-done
-
-CONTAINERNAME[4]=$tmp
-echo "Your www container $tmp is running"
-#run nginx 
-docker run -d --name $tmp --hostname $tmp -v $BASEDIR/nginx/conf.d/:/etc/nginx/conf.d/:rw -v /etc/localtime:/etc/localtime:ro --env CONSUL_HOST=$CONSUL_IP noonly/nginx
+	tmp=""
+	
+fi
 
 
-tmp=""
+read -p "Do you want to startup consul client server? [y/N]" y
 
-while [ "_$tmp" == '_' ]
-do
-        read -p "Please settings the mysql libuser server container name: " tmp
-	 echo ""
-        for exist in $CONTAINERNAME
-        do
-                if [ "x$tmp" == "x$exist" ]; then
+if [ "_$y" == "_y" ]; then
+	y=""
+	while [ "_$tmp" == '_' ]
+	do
+			read -p "Please settings the consul slave container name: " tmp
+			for exist in $CONTAINERNAME
+			do
+					if [ "x$tmp" == "x$exist" ]; then
 
-                        echo "invalid container name! please retry!!!"
-                        tmp=""
-                fi
-        done
+							echo "invalid container name! please retry!!!"
+							tmp=""
+					fi
+			done
 
-done
+	done
 
-CONTAINERNAME[5]=$tmp
-echo "Your libuser mysql container $tmp is running"
-#run mysql server
-docker run -d -v /var/lib/mysql/libuser/db:/var/lib/mysql -v /etc/localtime:/etc/localtime:ro -v /var/lib/mysql/libuser/mysql.json:/etc/consul.d/mysql.json \
--e MYSQL_ROOT_PASSWORD=123456 --name $tmp --hostname $tmp -e CONSUL_HOST=$CONSUL_IP noonly/mysql
+	index=$(($index+1))
+	CONTAINERNAME[$index]=$tmp
+
+	echo "Your consul slave container $tmp is running"
+	#run consul client for master node
+	docker run -d --name $tmp --hostname $tmp -v /etc/localtime:/etc/localtime:ro --env CONSUL_HOST=$CONSUL_IP noonly/consul
+
+	tmp=""
+fi
+
+read -p "Do you want to startup nginx server? [y/N]" y
+
+if [ "_$y" == "_y" ]; then
+	y=""
+	while [ "_$tmp" == '_' ]
+	do
+			read -p "Please settings the nginx server container name: " tmp
+		 echo ""
+			for exist in $CONTAINERNAME
+			do
+					if [ "x$tmp" == "x$exist" ]; then
+
+							echo "invalid container name! please retry!!!"
+							tmp=""
+					fi
+			done
+
+	done
+
+	index=$(($index+1))
+	CONTAINERNAME[$index]=$tmp
+	echo "Your www container $tmp is running"
+	#run nginx 
+	docker run -d --name $tmp --hostname $tmp -v $BASEDIR/nginx/conf.d/:/etc/nginx/conf.d/:rw -v /etc/localtime:/etc/localtime:ro --env CONSUL_HOST=$CONSUL_IP noonly/nginx
+
+
+	tmp=""
+fi
+
+read -p "Do you want to startup libuser mysql server? [y/N]" y
+
+if [ "_$y" == "_y" ]; then
+	y=""
+	while [ "_$tmp" == '_' ]
+	do
+			read -p "Please settings the mysql libuser server container name: " tmp
+		 echo ""
+			for exist in $CONTAINERNAME
+			do
+					if [ "x$tmp" == "x$exist" ]; then
+
+							echo "invalid container name! please retry!!!"
+							tmp=""
+					fi
+			done
+
+	done
+
+	index=$(($index+1))
+	CONTAINERNAME[$index]=$tmp
+	echo "Your libuser mysql container $tmp is running"
+	#run mysql server
+	docker run -d -v /var/lib/mysql/libuser/db:/var/lib/mysql -v /etc/localtime:/etc/localtime:ro -v /var/lib/mysql/libuser/mysql.json:/etc/consul.d/mysql.json \
+	-e MYSQL_ROOT_PASSWORD=123456 --name $tmp --hostname $tmp -e CONSUL_HOST=$CONSUL_IP noonly/mysql
+	tmp=""
+fi
