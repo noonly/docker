6615ebb9cb12c8fb24211345f20b9d29a43477fc
xiaole <x@noonly.com>
2016-07-09 10:26:25 +0800

    update app.sh fix start all

--- app.sh
+++ app.sh
@@ -77,7 +77,7 @@ export CONSUL_IP=$ip
 if [ "_${projectname[$tmp]}" == '_all' ]; then
 	echo "Starting all...."
 	j=0;
-	for pro in $project 
+	for pro in ${project[@]}; 
 	do
 		echo "${projectname[$j]} starting"
 		if [ "_${projectname[$j]}" != '_all' ]; then
