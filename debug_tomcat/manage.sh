b33c12c9e3b4d510a3b486c70762caaa51d980dd
xiaole <x@noonly.com>
2016-07-12 16:25:22 +0800

    edit  debug_tomcat/manage.sh

--- debug_tomcat/manage.sh
+++ debug_tomcat/manage.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
-
+process=`ps -ef | grep tomcat | grep -v "grep"`
 if [ -f "/tmp/project.conf" ]; then
-context=`cat /tmp/project.conf`
-if [ "_$context" != '_ok' ]; then
-#/usr/local/tomcat/bin/catalina.sh stop
-/usr/local/tomcat/bin/catalina.sh run
+	context=`cat /tmp/project.conf`
+	if [ "_$context" != '_ok' ]; then
+	#/usr/local/tomcat/bin/catalina.sh stop
+		if [ "_$process" == "_" ]; then
+			/usr/local/tomcat/bin/catalina.sh run
+		fi
+	else
+		if [ "_$process" != "_" ]; then
+			/usr/local/tomcat/bin/catalina.sh stop
+		fi
+	fi
 else
-/usr/local/tomcat/bin/catalina.sh stop
-fi
-else
-echo "Temp file not existed"
+	echo "Temp file not existed"
 fi
