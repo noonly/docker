b93fa4da670d85a35652ed2077f1324f4d97aa5b
xiaole <xiaole@edu.edu.noonly.com>
2016-07-18 15:04:58 +0800

    add nginx upload server

---
+++ nginx-upload/supervisor-3.3.0/setup.py
@@ -0,0 +1,89 @@
+##############################################################################
+#
+# Copyright (c) 2006-2013 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+import os
+import sys
+
+if sys.version_info[:2] < (2, 4) or sys.version_info[0] > 2:
+    msg = ("Supervisor requires Python 2.4 or later but does not work on "
+           "any version of Python 3.  You are using version %s.  Please "
+           "install using a supported version." % sys.version)
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+if sys.version_info[:2] < (2, 5):
+    # meld3 1.0.0 dropped python 2.4 support
+    # meld3 requires elementree on python 2.4 only
+    requires = ['meld3 >= 0.6.5 , < 1.0.0', 'elementtree']
+else:
+    requires = ['meld3 >= 0.6.5']
+
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except:
+    README = """\
+Supervisor is a client/server system that allows its users to
+control a number of processes on UNIX-like operating systems. """
+    CHANGES = ''
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: No Input/Output (Daemon)',
+    'Intended Audience :: System Administrators',
+    'Natural Language :: English',
+    'Operating System :: POSIX',
+    'Topic :: System :: Boot',
+    'Topic :: System :: Monitoring',
+    'Topic :: System :: Systems Administration',
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.4",
+    "Programming Language :: Python :: 2.5",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+]
+
+version_txt = os.path.join(here, 'supervisor/version.txt')
+supervisor_version = open(version_txt).read().strip()
+
+dist = setup(
+    name='supervisor',
+    version=supervisor_version,
+    license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
+    url='http://supervisord.org/',
+    description="A system for controlling process state under UNIX",
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=CLASSIFIERS,
+    author="Chris McDonough",
+    author_email="chrism@plope.com",
+    packages=find_packages(),
+    install_requires=requires,
+    extras_require={'iterparse': ['cElementTree >= 1.0.2']},
+    tests_require=['mock >= 0.5.0'],
+    include_package_data=True,
+    zip_safe=False,
+    namespace_packages=['supervisor'],
+    test_suite="supervisor.tests",
+    entry_points={
+        'console_scripts': [
+         'supervisord = supervisor.supervisord:main',
+         'supervisorctl = supervisor.supervisorctl:main',
+         'echo_supervisord_conf = supervisor.confecho:main',
+         'pidproxy = supervisor.pidproxy:main',
+        ],
+    },
+)
---
+++ nginx-upload/views.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+ 
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+ 
+UPLOAD_FILE_PATH = '/tmp/nginx_upload/'
+ 
+ 
+@csrf_exempt
+def upload(request):
+    request_params = request.POST
+    file_name = request_params['file_name']
+    file_content_type = request_params['file_content_type']
+    file_md5 = request_params['file_md5']
+    file_path = request_params['file_path']
+    file_size = request_params['file_size']
+ 
+    ip_address = request.META.get('HTTP_X_REAL_IP') or request.META.get('HTTP_REMOTE_ADD')
+ 
+    # save file to tmp
+    # new_file_name = '%s_%s' % (file_md5, ip_address)
+    new_file_path = ''.join([UPLOAD_FILE_PATH, file_md5, os.path.splitext(file_name)[-1]])
+    with open(new_file_path, 'a') as new_file:
+        with open(file_path, 'rb') as f:
+            new_file.write(f.read())
+ 
+    content = json.dumps({
+        'name': ''.join([file_md5,os.path.splitext(file_name)[-1]]),
+        'content_type': file_content_type,
+        'md5': file_md5,
+       # 'path': file_path,
+        'size': file_size,
+        'ip': ip_address,
+    })
+    response = HttpResponse(content, content_type='application/json; charset=utf-8')
+ 
+    return response
