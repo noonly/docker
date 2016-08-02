67d2897b7565aed57ee8f0af04dcab7517511b1c
xiaole <xiaole@noonly.com>
2016-08-02 12:37:12 +0800

    nginx 403

--- nginx-www/conf.d/access_by_redis.lua
+++ nginx-www/conf.d/access_by_redis.lua
@@ -2,10 +2,13 @@ local redis = require "resty.redis"
 local cache = redis.new()
 cache.connect(cache, '192.168.4.193', '6379')
 local res = cache:get(ngx.var.cookie_NOONLYSESSION)
-if res==ngx.null then
-    ngx.say("This is Null")
-    return
-end
+cache:set_timeout(1000)
+if res~=ngx.null then
 cache:expire(ngx.var.cookie_NOONLYSESSION,"600")
 ngx.say(res)
-cache:close()
+else
+ngx.say("NULL")
+end
+cache:set_keepalive(10000, 100)
+--cache:close()
+
--- nginx-www/conf.d/access_session.lua
+++ nginx-www/conf.d/access_session.lua
@@ -1,19 +1,29 @@
 uri = {"/Login/web","Friday","Saturday"}  
 for i,v in ipairs(uri) do 
-if (ngx.var.uri == v) then
-	return
-end
+	if (ngx.var.uri == v) then
+		return
+	end
 end 
 if (ngx.var.cookie_NOONLYSESSION ~= nil) then
 	local redis = require "resty.redis"
 	local cache = redis.new()
+	cache:set_timeout(1000)
 	cache.connect(cache, '192.168.4.193', '6379')
+	--cache.set_timeout(1000)
+	
+	cache:get_reused_times()
 	local res = cache:get(ngx.var.cookie_NOONLYSESSION)
 	if res~=ngx.null then
 		cache:expire(ngx.var.cookie_NOONLYSESSION,"1200")
 		ngx.req.set_header("user", res)
-		cache:close()
-		return
+		--cache:close()
+		--return
+	else
+		ngx.exit(403)
 	end
+	cache:set_keepalive(10000, 100)
+	--cache:close()
+	--cache:set_keepalive(10000, 100)
+else
+	ngx.exit(403)
 end
-ngx.exit(403)
