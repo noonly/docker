38cc3f4bff18e92ff90fe22ca42fe91df8d76afb
xiaole <xiaole@noonly.com>
2016-08-03 20:20:37 +0800

    no cache settings

--- nginx-www/conf.d/access_session.lua
+++ nginx-www/conf.d/access_session.lua
@@ -13,12 +13,15 @@ if (ngx.var.cookie_NOONLYSESSION ~= nil) then
 	
 	cache:get_reused_times()
 	local res = cache:get(ngx.var.cookie_NOONLYSESSION)
+	--ngx.log(ngx.ERR, res);
 	if res~=ngx.null then
 		cache:expire(ngx.var.cookie_NOONLYSESSION,"1200")
 		ngx.req.set_header("user", res)
 		--cache:close()
 		--return
+		--ngx.log(ngx.ERR,"ok")
 	else
+		--ngx.log(ngx.ERR,"no")
 		ngx.exit(403)
 	end
 	cache:set_keepalive(10000, 100)
