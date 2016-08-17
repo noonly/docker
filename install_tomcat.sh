5030de4e134f319ecaf7f98c4b59cabc558e5ec8
xiaole <xiaole@noonly.com>
2016-08-17 15:03:46 +0800

    test

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -31,10 +31,15 @@ do
 			do
 				cp $json $path/config/
 				sed -i "s/8080/$((8800+$c))/g" $path/config/$json
-				sed -i "s#123456#xiaole001#g" ./WEB-INF/classes/jdbcDataSource.xml
-				sed -i "s#libuser.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbcDataSource.xml
-				sed -i "s#libexam.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbcDataSource.xml
-				sed -i "s#libmessage.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbcDataSource.xml
+				sed -i "s#123456#xiaole001#g" ./WEB-INF/classes/jdbcDataSource.xml 2> /dev/null
+				sed -i "s#libuser.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbcDataSource.xml 2> /dev/null
+				sed -i "s#libexam.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbcDataSource.xml 2> /dev/null
+				sed -i "s#libmessage.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbcDataSource.xml 2> /dev/null
+
+				sed -i "s#123456#xiaole001#g" ./WEB-INF/classes/jdbc.properties 2> /dev/null
+				sed -i "s#libuser.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbc.properties 2> /dev/null
+				sed -i "s#libexam.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbc.properties 2> /dev/null
+				sed -i "s#libmessage.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbc.properties 2> /dev/null
 			done
 			cd $path
 			cp -R apache-tomcat-8.5.4 tomcat"_$folder"
