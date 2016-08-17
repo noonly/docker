15dd2103c686779daf1920f5d4669de1af6b1b02
xiaole <xiaole@noonly.com>
2016-08-17 08:29:48 +0800

    fix bug auto deploy tomcat update server.xml file

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -12,20 +12,26 @@ if [ ! -d "./apache-tomcat-8.5.4" ]; then
 	unzip apache-tomcat-8.5.4.zip
 fi
 path=`pwd`
-mkdir -p "$parh/config/"
+mkdir -p "$path/config/"
 c=1;
 for folder in `ls ./`
 do
 	if [ -d $path"/"$folder"/WebRoot" ]; then
 		if [ -f $path"/"$folder"/WebRoot/service.conf" ]; then
-			cp $path"/"$folder"/WebRoot/*.json $parh/config/"
+			y="y"
+                        read -p "Do you want to append $folder to localhost[Y/n]?" y
+                        if [ "$y" != "_y" ];then
+                                continue
+                        fi
+
+			cp "$path/$folder/WebRoot/*.json $path/config/"
 			cp -R apache-tomcat-8.5.4 tomcat"_$folder"
-			sed -i "s/Connector port=\"8080\"/Connector port=\"880$c\"/g" ./tomcat"_$c"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
-			sed -i "s/Server port=\"8005\"/Server port=\"700$c\"/g" ./tomcat"_$c"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
-			sed -i "s/Connector port=\"8009\"/Connector port=\"890$c\"/g" ./tomcat"_$c"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
-			p=$path"/"$folder"/WebRoot"
-			sed -i "s/ autoDeploy=\"true\">/<Context docBase=\"$p\" path="" reloadable=\"true\" source=\"$p\"/>/g" ./tomcat"_$c"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
-			c=$(($c-1))
+			sed -i "s/Connector port=\"8080\"/Connector port=\"880$c\"/g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
+			sed -i "s/Server port=\"8005\"/Server port=\"700$c\"/g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
+			sed -i "s/Connector port=\"8009\"/Connector port=\"890$c\"/g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
+			p="$path/$folder/WebRoot"
+			sed -i "s#autoDeploy=\"true\">#autoDeploy=\"true\"><Context docBase=\"$p\" path=\"\" reloadable=\"true\" source=\"$p\"\/>#g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
+			c=$(($c+1))
 		fi
 	fi
 done
