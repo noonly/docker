29ae9c936af19063c8dadf08353d65d986631806
xiaole <xiaole@noonly.com>
2016-08-17 13:48:12 +0800

    test

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -30,7 +30,7 @@ do
 			for json in `ls *.json`
 			do
 				cp $json $path/config/
-				sed -i "s/8080/880$c/g" $path/config/$json
+				sed -i "s/8080/$((8800+$c))/g" $path/config/$json
 				sed -i "s#123456#xiaole001#g" ./WEB-INF/classes/jdbcDataSource.xml
 				sed -i "s#libuser.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbcDataSource.xml
 				sed -i "s#libexam.service.dc1.consul#node1#g" ./WEB-INF/classes/jdbcDataSource.xml
@@ -38,9 +38,9 @@ do
 			done
 			cd $path
 			cp -R apache-tomcat-8.5.4 tomcat"_$folder"
-			sed -i "s/Connector port=\"8080\"/Connector port=\"880$c\"/g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
-			sed -i "s/Server port=\"8005\"/Server port=\"700$c\"/g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
-			sed -i "s/Connector port=\"8009\"/Connector port=\"890$c\"/g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
+			sed -i "s/Connector port=\"8080\"/Connector port=\"$((880+$c))\"/g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
+			sed -i "s/Server port=\"8005\"/Server port=\"$((700+$c))\"/g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
+			sed -i "s/Connector port=\"8009\"/Connector port=\"$((890+$c))\"/g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
 			p="$path/$folder/WebRoot"
 			sed -i "s#autoDeploy=\"true\">#autoDeploy=\"true\"><Context docBase=\"$p\" path=\"\" reloadable=\"true\" source=\"$p\"\/>#g" ./tomcat"_$folder"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
 			c=$(($c+1))
