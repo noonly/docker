f58cbed3bc65924b33f29ebb87ab4a4d01fd9687
xiaole <x@noonly.com>
2016-08-17 09:05:10 +0800

    git stash pop

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -1,16 +1,5 @@
 #! /bin/sh
 
-if [ ];then
-sudo apt-get install dnsmasq
-sudo mkdir -p /etc/dnsmasq.d/
-echo "server=/consul/127.0.0.1#8600" | sudo tee -a /etc/dnsmasq.d/10-consul
-fi
-if [ ! -e "./apache-tomcat-8.5.4.zip" ]; then
-	wget http://apache.fayea.com/tomcat/tomcat-8/v8.5.4/bin/apache-tomcat-8.5.4.zip
-fi
-if [ ! -d "./apache-tomcat-8.5.4" ]; then
-	unzip apache-tomcat-8.5.4.zip
-fi
 path=`pwd`
 mkdir -p "$path/config/"
 c=1;
@@ -20,7 +9,7 @@ do
 		if [ -f $path"/"$folder"/WebRoot/service.conf" ]; then
 			y="y"
                         read -p "Do you want to append $folder to localhost[Y/n]?" y
-                        if [ "$y" != "_y" ];then
+                        if [ "_$y" != "_y" ];then
                                 continue
                         fi
 
@@ -35,21 +24,10 @@ do
 		fi
 	fi
 done
-if [ ]; then
-c=`ls $parh/config/*.json | wc -l`
-#read -p "How match tomcat do you want to startup[>=1]?" c
-while [ "_$c" != "_0" ]
-do
-	cp -R apache-tomcat-8.5.4 tomcat"_$c"
-	sed -i "s/Connector port=\"8080\"/Connector port=\"880$c\"/g" ./tomcat"_$c"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
-	sed -i "s/Server port=\"8005\"/Server port=\"700$c\"/g" ./tomcat"_$c"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
-	sed -i "s/Connector port=\"8009\"/Connector port=\"890$c\"/g" ./tomcat"_$c"/conf/server.xml #`grep -tl ./tomcat"_$c"/config/server.xml`
-	c=$(($c-1))
-done
-fi
+
 y="y"
 read -p "Do you want to startup consul service[Y/n]?" y
-if [ "_$y" == "_y" ];then
+if [ "_$y" == "_y" ]; then
 wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
 unzip consul_0.6.4_linux_amd64.zip
 sudo mkdir -p /etc/consul.d
