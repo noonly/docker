c90002158e70041bf14282b772d76debd8ea86aa
xiaole <xiaole@noonly.com>
2016-08-18 20:11:06 +0800

    add my profile file

--- nginx-www/conf.d/access_session.lua
+++ nginx-www/conf.d/access_session.lua
@@ -1,4 +1,4 @@
-uri = {"/Login/web","/admin/login.html"}  
+uri = {"/myprofile","/zuxia/exam.html","/Login/regster","/admin/register.html","/appMessage/hotnews","/appMessage/QueryCircleMsg","/appMessage/rdcircle","/zuxia/member.html","/zuxia/app.html","/zuxia/play.html","/Video/response","/Video/typeInfo","/Video/type","/Video/limitType","/zuxia/video.html","/zuxia/circle.html","/zuxia/index.html","/Login/web","/admin/login.html","/Back/BackOne","/Back/BackTwo","/Back/BackThree","/Back/BackFour"} 
 for i,v in ipairs(uri) do 
 	if (ngx.var.uri == v) then
 		return
@@ -13,15 +13,12 @@ if (ngx.var.cookie_NOONLYSESSION ~= nil) then
 	
 	cache:get_reused_times()
 	local res = cache:get(ngx.var.cookie_NOONLYSESSION)
-	--ngx.log(ngx.ERR, res);
 	if res~=ngx.null then
 		cache:expire(ngx.var.cookie_NOONLYSESSION,"1200")
 		ngx.req.set_header("user", res)
 		--cache:close()
 		--return
-		--ngx.log(ngx.ERR,"ok")
 	else
-		--ngx.log(ngx.ERR,"no")
 		ngx.exit(403)
 	end
 	cache:set_keepalive(10000, 100)
--- nginx-www/conf.d/rewitre_token_check.lua
+++ nginx-www/conf.d/rewitre_token_check.lua
@@ -2,8 +2,7 @@ local random = ngx.var.cookie_NOONLY_KEY
 if (random == nil) then
     return ngx.redirect("/auth_token?url=" .. ngx.var.request_uri)
 end
-local token = ngx.md5("Ntoken" .. random .. ngx.var.remote_addr .. random)
+local token = ngx.md5("Ntoken" .. random .. "NT123456NT" .. random)
 if (ngx.var.cookie_NOONLY_ACCESS ~= token) then
     return ngx.redirect("/auth_token?url=".. ngx.var.request_uri)
 end
-
