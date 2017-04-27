f637957718cc9ea3dd050daaefedba3a6516eb76
xiaole <xiaole@noonly.com>
2017-04-27 20:20:12 +0800

    add vpn/l2top

---
+++ vpn/README.md
@@ -0,0 +1,141 @@
+# IPsec VPN Server on Docker
+
+[![Docker Stars](https://img.shields.io/docker/stars/fcojean/l2tp-ipsec-vpn-server.svg)](https://hub.docker.com/r/fcojean/l2tp-ipsec-vpn-server) 
+[![Docker Pulls](https://img.shields.io/docker/pulls/fcojean/l2tp-ipsec-vpn-server.svg)](https://hub.docker.com/r/fcojean/l2tp-ipsec-vpn-server)
+
+Docker image to run an IPsec VPN server, with support for both `IPsec/L2TP` and `IPsec/XAuth ("Cisco IPsec")`.
+
+Based on Debian Jessie with [Libreswan](https://libreswan.org) (IPsec VPN software) and [xl2tpd](https://github.com/xelerance/xl2tpd) (L2TP daemon).
+
+This docker image is based on [Lin Song work](https://github.com/hwdsl2/docker-ipsec-vpn-server) and adds those features:
+
+* Multiple VPN users declaration support
+* Native NAT Transversal support
+* No waiting time before a user can reconnect in case of disconnection support
+* Custom network interface support 
+
+## Install Docker
+
+Follow [these instructions](https://docs.docker.com/engine/installation/) to get Docker running on your server.
+
+## Download
+
+Get the trusted build from the [Docker Hub registry](https://hub.docker.com/r/fcojean/l2tp-ipsec-vpn-server):
+
+```
+docker pull fcojean/l2tp-ipsec-vpn-server
+```
+
+or download and compile the source yourself from GitHub:
+
+```
+git clone https://github.com/fcojean/l2tp-ipsec-vpn-server.git
+cd l2tp-ipsec-vpn-server
+docker build -t fcojean/l2tp-ipsec-vpn-server .
+```
+
+## How to use this image
+
+### Environment variables
+
+This Docker image uses the following two environment variables, that can be declared in an `env` file (see vpn.env.example file):
+
+```
+VPN_IPSEC_PSK=<IPsec pre-shared key>
+VPN_USER_CREDENTIAL_LIST=[{"login":"userTest1","password":"test1"},{"login":"userTest2","password":"test2"}]
+VPN_NETWORK_INTERFACE=eth0
+```
+
+* `VPN_IPSEC_PSK` : The IPsec PSK (pre-shared key).
+* `VPN_USER_CREDENTIAL_LIST` : Multiple users VPN credentials list. Users login and password must be defined in a json format array. Each user should be define with a "login" and a "password" attribute.
+* `VPN_NETWORK_INTERFACE` : The network interface name (eth0 by default).
+
+**Note:** In your `env` file, DO NOT put single or double quotes around values, or add space around `=`. Also, DO NOT use these characters within values: `\ " '`
+
+All the variables to this image are optional, which means you don't have to type in any environment variable, and you can have an IPsec VPN server out of the box! Read the sections below for details.
+
+### Start the IPsec VPN server
+
+VERY IMPORTANT ! First, run this command on the Docker host to load the IPsec `NETKEY` kernel module:
+
+```
+sudo modprobe af_key
+```
+
+Start a new Docker container with the following command (replace `./vpn.env` with your own `env` file) :
+
+```
+docker run \
+    --name l2tp-ipsec-vpn-server \
+    --env-file ./vpn.env \
+    -p 500:500/udp \
+    -p 4500:4500/udp \
+    -v /lib/modules:/lib/modules:ro \
+    -d --privileged \
+    fcojean/l2tp-ipsec-vpn-server
+```
+
+### Retrieve VPN login details
+
+If you did not set environment variables via an `env` file, a vpn user login will default to `vpnuser` and both `VPN_IPSEC_PSK` and vpn user password will be randomly generated. To retrieve them, show the logs of the running container:
+
+```
+docker logs l2tp-ipsec-vpn-server
+```
+
+Search for these lines in the output:
+
+```console
+Connect to your new VPN with these details:
+
+Server IP: <VPN Server IP>
+IPsec PSK: <IPsec pre-shared key>
+Users credentials :
+Login : <vpn user_login_1> Password : <vpn user_password_1>
+...
+Login : <vpn user_login_N> Password : <vpn user_password_N>
+```
+
+### Check server status
+
+To check the status of your IPsec VPN server, you can pass `ipsec status` to your container like this:
+
+```
+docker exec -it l2tp-ipsec-vpn-server ipsec status
+```
+
+## Next Steps
+
+Get your computer or device to use the VPN. Please refer to:
+
+[Configure IPsec/L2TP VPN Clients](https://github.com/hwdsl2/setup-ipsec-vpn/blob/master/docs/clients.md)   
+[Configure IPsec/XAuth VPN Clients](https://github.com/hwdsl2/setup-ipsec-vpn/blob/master/docs/clients-xauth.md)
+
+Enjoy your very own VPN! :sparkles::tada::rocket::sparkles:
+
+## Technical Details
+
+There are two services running: `Libreswan (pluto)` for the IPsec VPN, and `xl2tpd` for L2TP support.
+
+Clients are configured to use [Google Public DNS](https://developers.google.com/speed/public-dns/) when the VPN connection is active.
+
+The default IPsec configuration supports:
+
+* IKEv1 with PSK and XAuth ("Cisco IPsec")
+* IPsec/L2TP with PSK
+
+The ports that are exposed for this container to work are:
+
+* 4500/udp and 500/udp for IPsec
+
+## Author
+
+Copyright (C) 2016 François COJEAN
+
+## License
+
+Based on [the work of Lin Song](https://github.com/hwdsl2/docker-ipsec-vpn-server) (Copyright 2016)   
+Based on [the work of Thomas Sarlandie](https://github.com/sarfata/voodooprivacy) (Copyright 2012)
+
+This work is licensed under the [Creative Commons Attribution-ShareAlike 3.0 Unported License](http://creativecommons.org/licenses/by-sa/3.0/)   
+Attribution required: please include my name in any derivative and let me know how you have improved it !
---
+++ vpn/run.sh
@@ -0,0 +1,263 @@
+#!/bin/bash
+#
+# Docker script to configure and start an IPsec VPN server
+#
+# DO NOT RUN THIS SCRIPT ON YOUR PC OR MAC! THIS IS ONLY MEANT TO BE RUN
+# IN A DOCKER CONTAINER!
+#
+# Copyright (C) 2016 Lin Song
+# Based on the work of Thomas Sarlandie (Copyright 2012)
+#
+# This work is licensed under the Creative Commons Attribution-ShareAlike 3.0
+# Unported License: http://creativecommons.org/licenses/by-sa/3.0/
+#
+# Attribution required: please include my name in any derivative and let me
+# know how you have improved it!
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+if [ ! -f /.dockerenv ]; then
+  echo 'This script should ONLY be run in a Docker container! Aborting.'
+  exit 1
+fi
+
+if [ -z "$VPN_IPSEC_PSK" ] && [ -z "$VPN_USER_CREDENTIAL_LIST" ]; then
+  VPN_IPSEC_PSK="$(< /dev/urandom tr -dc 'A-HJ-NPR-Za-km-z2-9' | head -c 16)"
+  VPN_PASSWORD="$(< /dev/urandom tr -dc 'A-HJ-NPR-Za-km-z2-9' | head -c 16)"
+  VPN_USER_CREDENTIAL_LIST="[{\"login\":\"vpnuser\",\"password\":\"$VPN_PASSWORD\"}]"
+fi
+
+if [ -z "$VPN_IPSEC_PSK" ] || [ -z "$VPN_USER_CREDENTIAL_LIST" ]; then
+  echo "VPN credentials must be specified. Edit your 'env' file and re-enter them."
+  exit 1
+fi
+
+if [ -z "$VPN_NETWORK_INTERFACE" ]; then
+  VPN_NETWORK_INTERFACE="eth0"
+fi
+
+if [ `cat /sys/class/net/$VPN_NETWORK_INTERFACE/operstate` != "up" ]; then
+  echo "Network interface '$VPN_NETWORK_INTERFACE' is not available. Aborting."
+  exit 1
+fi
+
+
+echo
+echo 'Trying to auto discover IPs of this server...'
+echo
+
+# In case auto IP discovery fails, you may manually enter the public IP
+# of this server in your 'env' file, using variable 'VPN_PUBLIC_IP'.
+PUBLIC_IP=${VPN_PUBLIC_IP:-''}
+
+# Try to auto discover server IPs
+[ -z "$PUBLIC_IP" ] && PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+[ -z "$PUBLIC_IP" ] && PUBLIC_IP=$(wget -t 3 -T 15 -qO- http://ipv4.icanhazip.com)
+PRIVATE_IP=$(ip -4 route get 1 | awk '{print $NF;exit}')
+[ -z "$PRIVATE_IP" ] && PRIVATE_IP=$(ifconfig $VPN_NETWORK_INTERFACE | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
+
+# Check IPs for correct format
+IP_REGEX="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+if ! printf %s "$PUBLIC_IP" | grep -Eq "$IP_REGEX"; then
+  echo "Cannot find valid public IP. Please manually enter the public IP"
+  echo "of this server in your 'env' file, using variable 'VPN_PUBLIC_IP'."
+  exit 1
+fi
+if ! printf %s "$PRIVATE_IP" | grep -Eq "$IP_REGEX"; then
+  echo "Cannot find valid private IP. Aborting."
+  exit 1
+fi
+
+# Create IPsec (Libreswan) config
+cat > /etc/ipsec.conf <<EOF
+version 2.0
+
+config setup
+
+  nat_traversal=yes
+  virtual_private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12,%v4:!192.168.42.0/23
+  protostack=netkey
+  nhelpers=0
+  interfaces=%defaultroute
+  uniqueids=no
+
+conn shared
+  left=$PRIVATE_IP
+  leftid=$PUBLIC_IP
+  right=%any
+  forceencaps=yes
+  authby=secret
+  pfs=no
+  rekey=no
+  keyingtries=3
+  dpddelay=15
+  dpdtimeout=30
+  dpdaction=clear
+  ike=3des-sha1,aes-sha1
+  phase2alg=3des-sha1,aes-sha1
+
+conn l2tp-psk
+  auto=add
+  leftsubnet=$PRIVATE_IP/32
+  leftnexthop=%defaultroute
+  leftprotoport=17/1701
+  rightprotoport=17/%any
+  type=transport
+  auth=esp
+  also=shared
+
+conn xauth-psk
+  auto=add
+  leftsubnet=0.0.0.0/0
+  rightaddresspool=192.168.43.10-192.168.43.250
+  modecfgdns1=8.8.8.8
+  modecfgdns2=114.114.114.114
+  leftxauthserver=yes
+  rightxauthclient=yes
+  leftmodecfgserver=yes
+  rightmodecfgclient=yes
+  modecfgpull=yes
+  xauthby=file
+  ike-frag=yes
+  ikev2=never
+  cisco-unity=yes
+  also=shared
+EOF
+
+# Specify IPsec PSK
+cat > /etc/ipsec.secrets <<EOF
+$PUBLIC_IP  %any  : PSK "$VPN_IPSEC_PSK"
+EOF
+
+# Create xl2tpd config
+cat > /etc/xl2tpd/xl2tpd.conf <<EOF
+[global]
+port = 1701
+
+[lns default]
+ip range = 192.168.42.10-192.168.42.50
+local ip = 192.168.42.1
+require chap = yes
+refuse pap = yes
+require authentication = yes
+name = l2tpd
+pppoptfile = /etc/ppp/options.xl2tpd
+length bit = yes
+EOF
+
+# Set xl2tpd options
+cat > /etc/ppp/options.xl2tpd <<EOF
+ipcp-accept-local
+ipcp-accept-remote
+ms-dns 8.8.8.8
+ms-dns 114.114.114.114
+noccp
+auth
+crtscts
+lock
+lcp-echo-failure 4
+lcp-echo-interval 30
+EOF
+
+# Create VPN credentials
+echo "$VPN_USER_CREDENTIAL_LIST" | jq -r '.[] | .login + " l2tpd " + .password + " *"' > /etc/ppp/chap-secrets
+
+CREDENTIALS_NUMBER=`echo "$VPN_USER_CREDENTIAL_LIST" | jq 'length'`
+for (( i=0; i<=$CREDENTIALS_NUMBER - 1; i++ ))
+do
+	VPN_USER_LOGIN=`echo "$VPN_USER_CREDENTIAL_LIST" | jq ".["$i"] | .login"`
+	VPN_USER_PASSWORD=`echo "$VPN_USER_CREDENTIAL_LIST" | jq ".["$i"] | .password"`
+	VPN_PASSWORD_ENC=$(openssl passwd -1 "$VPN_USER_PASSWORD")
+	echo "${VPN_USER_LOGIN}:${VPN_PASSWORD_ENC}:xauth-psk" >> /etc/ipsec.d/passwd
+done
+
+# Update sysctl settings
+if ! grep -qs "Added by run.sh script" /etc/sysctl.conf; then
+cat >> /etc/sysctl.conf <<EOF
+
+# Added by run.sh script
+kernel.msgmnb = 65536
+kernel.msgmax = 65536
+kernel.shmmax = 68719476736
+kernel.shmall = 4294967296
+
+net.ipv4.ip_forward = 1
+net.ipv4.tcp_syncookies = 1
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+net.ipv4.conf.lo.send_redirects = 0
+net.ipv4.conf.$VPN_NETWORK_INTERFACE.send_redirects = 0
+net.ipv4.conf.all.rp_filter = 0
+net.ipv4.conf.default.rp_filter = 0
+net.ipv4.conf.lo.rp_filter = 0
+net.ipv4.conf.$VPN_NETWORK_INTERFACE.rp_filter = 0
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+
+net.core.wmem_max = 12582912
+net.core.rmem_max = 12582912
+net.ipv4.tcp_rmem = 10240 87380 12582912
+net.ipv4.tcp_wmem = 10240 87380 12582912
+EOF
+fi
+
+# Create IPTables rules
+iptables -I INPUT 1 -p udp -m multiport --dports 500,4500 -j ACCEPT
+iptables -I INPUT 2 -p udp --dport 1701 -m policy --dir in --pol ipsec -j ACCEPT
+iptables -I INPUT 3 -p udp --dport 1701 -j DROP
+iptables -I FORWARD 1 -m conntrack --ctstate INVALID -j DROP
+iptables -I FORWARD 2 -i eth+ -o ppp+ -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -I FORWARD 3 -i ppp+ -o eth+ -j ACCEPT
+iptables -I FORWARD 4 -i eth+ -d 192.168.43.0/24 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -I FORWARD 5 -s 192.168.43.0/24 -o eth+ -j ACCEPT
+iptables -t nat -I POSTROUTING -s 192.168.43.0/24 -o eth+ -m policy --dir out --pol none -j SNAT --to-source "$PRIVATE_IP"
+iptables -t nat -I POSTROUTING -s 192.168.42.0/24 -o eth+ -j SNAT --to-source "$PRIVATE_IP"
+
+# Reload sysctl.conf
+sysctl -q -p 2>/dev/null
+
+# Update file attributes
+chmod 600 /etc/ipsec.secrets /etc/ppp/chap-secrets /etc/ipsec.d/passwd
+
+cat <<EOF
+
+================================================
+
+IPsec VPN server is now ready for use!
+
+Connect to your new VPN with these details:
+
+Server IP: $PUBLIC_IP
+IPsec PSK: $VPN_IPSEC_PSK
+Users credentials :
+EOF
+
+for (( i=0; i<=$CREDENTIALS_NUMBER - 1; i++ ))
+do
+	VPN_USER_LOGIN=`echo "$VPN_USER_CREDENTIAL_LIST" | jq -r ".["$i"] | .login"`
+	VPN_USER_PASSWORD=`echo "$VPN_USER_CREDENTIAL_LIST" | jq -r ".["$i"] | .password"`
+	echo "Login : ${VPN_USER_LOGIN} Password : ${VPN_USER_PASSWORD}"
+done
+
+cat <<EOF
+
+Write these down. You'll need them to connect!
+
+Setup VPN Clients: https://git.io/vpnclients
+
+================================================
+
+EOF
+
+# Load IPsec NETKEY kernel module
+modprobe af_key
+
+# Start services
+mkdir -p /var/run/pluto /var/run/xl2tpd
+rm -f /var/run/pluto/pluto.pid /var/run/xl2tpd.pid
+
+/usr/local/sbin/ipsec start --config /etc/ipsec.conf
+exec /usr/sbin/xl2tpd -D -c /etc/xl2tpd/xl2tpd.conf
