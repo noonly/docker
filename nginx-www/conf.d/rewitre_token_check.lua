aa1c9f2ab527c90934d1f2247e12bdd9ad8b8780
pibo <xiaole@noonly.com>
2017-05-02 11:43:20 +0800

    nginx updated

--- nginx-www/conf.d/rewitre_token_check.lua
+++ nginx-www/conf.d/rewitre_token_check.lua
@@ -6,8 +6,7 @@ if (random == nil) or (ngx.var.cookie_NOONLY_ACCESS ~= ngx.md5("Ntoken" .. rando
         end
         local token = ngx.md5("Ntoken" .. random .. "NT123456NT" .. random)
         if (ngx.var.cookie_NOONLY_ACCESS ~= token) then
-
-                ngx.header["Set-Cookie"] = {"NOONLY_ACCESS=" .. token, "NOONLY_KEY=" .. random}
+                ngx.header["Set-Cookie"] = {"NOONLY_ACCESS=" .. token.."; path=/", "NOONLY_KEY=" .. random.."; path=/"}
         end
 
 end
